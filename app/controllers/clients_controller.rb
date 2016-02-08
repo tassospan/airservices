a84de0eefdea445f0fe2ba90bace7004baa6c902
obiuno <fifart@gmail.com>
2016-02-08 21:43:03 +0000

    client form on front page added

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -15,7 +15,7 @@ class ClientsController < ApplicationController
   def create
   @client = Client.new(client_params)
   if @client.save
-      redirect_to @client
+      redirect_to root_path
   else
       render 'new'
   end
--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -1,8 +1,34 @@
 class FrontpageController < ApplicationController
+    before_action :find_client, only: [:show, :edit, :update, :destroy]
+    
     def index
         @box_one = Welcome.first
         @box_two = Welcome.second
         @box_three = Welcome.last
         @social = Social.first
     end
+    
+    def new
+        @client = Client.new
+    end
+  
+    def create
+        @client = Client.new(client_params)
+        if @client.save
+        redirect_to root_path
+        else
+        render 'new'
+        end
+    end
+    
+    private
+  
+    def find_client
+        @client = Client.find(params[:id])
+    end
+  
+    def client_params
+        params.require(:client).permit(:name, :surname, :email, :phone, :address, :floor, :message)
+    end
+    
 end
