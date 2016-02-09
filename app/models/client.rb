26a75e6fe57973fdae27b343a5aade76dfda4288
obiuno <fifart@gmail.com>
2016-02-09 12:23:38 +0000

    finished with validations & style modifications

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,8 @@
 class Client < ActiveRecord::Base
-   
+   validates_presence_of :name
+   validates_presence_of :surname
+   validates_presence_of :email 
+   validates_presence_of :phone
+   validates_uniqueness_of :email 
+   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
