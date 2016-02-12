852e2ecfc17496c218271d6439fcdcf12053b869
obiuno <fifart@gmail.com>
2016-02-12 09:00:27 +0000

    images fixed letter name required

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,4 @@
 class Property < ActiveRecord::Base
-    has_attached_file :image, styles: { medium: "450x300>", thumb: "100x100>" }
+    has_attached_file :image, styles: { medium: "500x340>", thumb: "100x100>" }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
